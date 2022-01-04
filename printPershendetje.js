ee1da7dd152cab74d8a5f9db575c37ee582580a3
bindlimani <bindlimani1@gmail.com>
2022-01-04 12:06:24 +0100

    Fixed login functionality

--- printPershendetje.js
+++ printPershendetje.js
@@ -1,6 +1,25 @@
 //alert(localStorage.getItem("username"));
         //alert("Session=" + sessionStorage.getItem("username"));
-        if(sessionStorage.getItem("username") != null)
+        if(sessionStorage.getItem("username") != null) {
             document.getElementById("pershendetje").innerHTML = "Pershendetje " + sessionStorage.getItem("username");
-        else
+            document.getElementById("log-in").innerHTML = "Pershendetje " + sessionStorage.getItem("username") + '<span onclick="logout()" style="color:white;"> (Shkyqu)</span>';
+        }
+        else if(localStorage.getItem("username") != null){
             document.getElementById("pershendetje").innerHTML = "Pershendetje " + localStorage.getItem("username");
+            document.getElementById("log-in").innerHTML = "Pershendetje " + localStorage.getItem("username") + '<span onclick="logout()" style="color:white;"> (Shkyqu)</span>';
+        }
+        else {
+            document.getElementById("log-in").innerText = "Kycu";
+        }
+
+function logout() {
+    document.getElementById("log-in").innerText = "Kycu";
+    if(sessionStorage.getItem("username") != null) {
+        sessionStorage.removeItem("username");
+        alert("Shkyqja u be me sukses");
+    }
+    else {
+        localStorage.removeItem("username");
+        alert("Shkyqja u be me sukses");
+    }
+}
